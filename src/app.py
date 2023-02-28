04b64eed403f0577644b8e8ee3c6fa04c8bf328e
ecam-b <ec.angaritas@gmail.com>
2023-02-28 10:24:09 -0500

    Empezando a trabajar con Flasgger: Modificación en el requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flasgger import Swagger
 # database
 from database.db import db
 # config
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI_CONNECTION
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 SQLAlchemy(app)
 Marshmallow(app)
+swagger = Swagger(app)
 
 # inicio de db
 db.init_app(app)
