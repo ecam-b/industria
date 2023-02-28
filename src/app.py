180ecf5d31fab5ac72f57c51b6c1e6c33958ef38
ecam-b <ec.angaritas@gmail.com>
2023-02-28 08:53:13 -0500

    Configuración de la primera entidad, apunto de probar.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+# database
+from database.db import db
 # config
 from config import DATABASE_URI_CONNECTION, SECRET_KEY
+# import Blueprints
+from routes import Usuario
 
 app = Flask(__name__)
 
@@ -12,8 +16,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 SQLAlchemy(app)
 Marshmallow(app)
 
-
+# inicio de db
+db.init_app(app)
+with app.app_context():
+  db.create_all()
 
 
 if __name__ == "__main__":
+  # import Blueprints
+  app.register_blueprint(Usuario.usuario_bp, url_prefix="/usuario")
+
   app.run(debug=True)
