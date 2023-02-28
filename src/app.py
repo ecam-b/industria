36b3dad0a837b1e655f7e5b5a228c3a20d3e06e9
ecam-b <ec.angaritas@gmail.com>
2023-02-28 08:37:14 -0500

    Se establecido la conexión a la base de datos

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# config
+from config import DATABASE_URI_CONNECTION, SECRET_KEY
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI_CONNECTION
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLAlchemy(app)
+
+
 
 
 if __name__ == "__main__":
